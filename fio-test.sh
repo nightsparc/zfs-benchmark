87c22fe98e4d72de330da315d468f41eab675fb2
nightsparc <17517082+nightsparc@users.noreply.github.com>
2021-03-03 14:29:00 +0100

    Added fio job file and small wrapper script.

---
+++ fio-test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+
+## @brief test io for a specified ashift width
+function test_io (){
+    local ashift="$1"
+    export TEST_NAME="ashift$ashift"
+    export FIO_DIR="/rpool$ashift/test"
+
+    fio fio-ashift-eval.ini
+}
+
+# Test for ashift9
+test_io 9
+
+# Test for ashift12
+test_io 12
+
+# Test for ashift13
+test_io 13
